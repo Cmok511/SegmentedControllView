f72a5a79d1deec490d6c8112bef7d0f8f3aeb65c
Cmok511 <cmok511@icloud.com>
2023-10-27 00:27:51 +0300

    first

---
+++ SegmentedControllView/SegmentedControllView.h
@@ -0,0 +1,18 @@
+//
+//  SegmentedControllView.h
+//  SegmentedControllView
+//
+//  Created by Денис Чупров on 27.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SegmentedControllView.
+FOUNDATION_EXPORT double SegmentedControllViewVersionNumber;
+
+//! Project version string for SegmentedControllView.
+FOUNDATION_EXPORT const unsigned char SegmentedControllViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SegmentedControllView/PublicHeader.h>
+
+
